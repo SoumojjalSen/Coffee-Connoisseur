85de865e1bbe984034f01a33d03d040798411e85
SoumojjalSen <soumojjalsen@gmail.com>
2023-12-20 04:21:23 +0530

    Font changes and code refactor

--- components/banner.js
+++ components/banner.js
@@ -3,13 +3,17 @@ import styles from "./static_css/Banner.module.css";
 import Image from "next/image";
 import { Pattaya } from "next/font/google";
 
+const pattaya = Pattaya({ subsets: ["latin"], display: "swap", weight: "400" });
+
 const Banner = (props) => {
   return (
     <div className={styles.super_container}>
       <div className={styles.container}>
-        <div className={styles.title}>
-          <span className={styles.title1}>Coffee</span>
-          <span className={styles.title2}>Connoisseur</span>
+        <div className={pattaya.className}>
+          <div className={styles.title}>
+            <span className={styles.title1}>Coffee</span>
+            <span className={styles.title2}>Connoisseur</span>
+          </div>
         </div>
         <p className={styles.subTitle}>Discover your local coffee shops</p>
         <div className={styles.buttonWrapper}>
--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <link rel="preload" href="/fonts/Pattaya-Regular.ttf" as='font' crossOrigin='anonymous  '></link> */}
-        <link rel="preload" href="/fonts/IBMPlexSerif-Regular.ttf" as='font' crossOrigin='anonymous  '></link>
-        {/* <link rel="preload" href="/fonts/IBMPlexSerif-Bold.ttf" as='font' crossOrigin='anonymous  '></link> */}
-        {/* <link rel="preload" href="/fonts/IBMPlexSerif-SemiBold.ttf" as='font' crossOrigin='anonymous  '></link> */}
-
       </Head>
       <body>
         <Main />
